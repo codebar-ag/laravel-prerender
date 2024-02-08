456f810acba188acfa61bb9c04de4a948c53580d
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2024-02-08 16:51:33 +0000

    WIP

--- src/LaravelPrerenderServiceProvider.php
+++ src/LaravelPrerenderServiceProvider.php
@@ -13,7 +13,7 @@ class LaravelPrerenderServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/../config/prerender.php' => config_path('prerender.php'),
+            __DIR__.'/../config/prerender.php' => config_path('prerender.php'),
         ], 'config');
 
         if (config('prerender.enable')) {
@@ -30,7 +30,7 @@ class LaravelPrerenderServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/prerender.php',
+            __DIR__.'/../config/prerender.php',
             'prerender'
         );
     }
--- tests/PrerenderMiddlewareTest.php
+++ tests/PrerenderMiddlewareTest.php
@@ -34,7 +34,7 @@ class PrerenderMiddlewareTest extends TestCase
     /** @test */
     public function it_should_prerender_when_user_agent_is_part_of_crawler_user_agents()
     {
-        $this->get('/test-middleware', ['User-Agent' => 'Googlebot/2.1 (+http://www.google.com/bot.html)',])
+        $this->get('/test-middleware', ['User-Agent' => 'Googlebot/2.1 (+http://www.google.com/bot.html)'])
             ->assertHeader('prerender.io-mock', true)
             ->assertSuccessful();
     }
